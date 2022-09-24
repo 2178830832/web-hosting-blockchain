f81d16c8453cc9b5a4eb10156c8cb0bf3f78b364
scyyc7 <scyyc7@nottingham.edu.cn>
2022-09-24 11:08:55 +0800

    Setting up Admin & Client application

---
+++ service-admin/src/test/java/org/nottingham/serviceadmin/ServiceAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nottingham.serviceadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceAdminApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
